444ad554ed9ffdb945230c6973e12a2f7c02d7b9
Alex <sl2261981@gmail.com>
2025-10-29 13:22:06 +0300

    Домашка до 1 ноября

--- 1_november/merge_sort.h
+++ 1_november/merge_sort.h
@@ -4,6 +4,5 @@
 void merge(int arr[], int left, int mid, int right);
 void mergeSort(int arr[], int left, int right);
 void mergeSort(int arr[], int size);
-void printArray(int arr[], int size);
 
 #endif
